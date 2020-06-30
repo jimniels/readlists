1f0aaa69a60de160594bd7ec4a141d219352b439
Jim Nielsen <jimniels@gmail.com>
2020-06-30 08:42:43 -0600

    refactor: update epub func

--- functions/epub/epub.js
+++ functions/epub/epub.js
@@ -1,44 +1,11 @@
-// @TODO have the client POST json of the list, so we dont' have to bring
-// dropbox auth into here. Then this merely does the conversion to epub and downloads
-// https://stackoverflow.com/questions/53297978/amazon-lambda-return-docx-file-node-js
-
 const Epub = require("epub-gen");
 const fs = require("fs");
 
-// @TODO where to output the file? to user's dropbox? where, just a temp folder?
-
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
-  // @TODO check for correct query params
-  const book = JSON.parse(event.body);
-
-  // if (!listId) {
-  //   return {
-  //     statusCode: 400,
-  //     body:
-  //       "Invalid request. URL paramter is required, i.e. `?list-id=123456789...`",
-  //   };
-  // }
-
+  // https://stackoverflow.com/questions/53297978/amazon-lambda-return-docx-file-node-js
   try {
-    // const list = await getList(listId);
-    // const listArticleContents = await Promise.all(
-    //   list.articles.map((article) => getArticle(article.id))
-    // );
-
-    // const options = {
-    //   title: list.title,
-    //   author: "Readlist", // @TODO user
-    //   publisher: "Readlist",
-    //   // cover, // @TODO image cover?
-    //   content: list.articles.map((article, i) => {
-    //     return {
-    //       title: article.title,
-    //       ...(article.author ? { author: article.author } : {}),
-    //       data: listArticleContents[i], // local image paths?
-    //     };
-    //   }),
-    // };
+    const book = JSON.parse(event.body);
 
     await new Epub(book, "./readlist.epub").promise.catch((e) => {
       console.log(e);
