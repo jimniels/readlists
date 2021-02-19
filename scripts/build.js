f0a203bc2ffc03564ba4773934c18a16127b2340
Jim Nielsen <jimniels@gmail.com>
2021-02-19 09:16:00 -0700

    fix: build script

--- scripts/build.js
+++ scripts/build.js
@@ -4,7 +4,6 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { execSync } from "child_process";
 const __dirname = path.join(path.dirname(fileURLToPath(import.meta.url)));
-const INDEX_PATH = path.join(__dirname, "../build/index.html");
 
 const { version } = JSON.parse(
   fs.readFileSync(path.join(__dirname, "../package.json"))
@@ -13,11 +12,14 @@ const sha = execSync("git rev-parse HEAD").toString().trim().slice(0, 7);
 
 console.log(`${version}@${sha}`);
 
-let index = fs.readFileSync(INDEX_PATH).toString();
+const indexPath = path.join(__dirname, "../build/index.html");
+let index = fs.readFileSync(indexPath).toString();
 index = index.replace("__VERSION__", `${version}@${sha}`);
+fs.writeFileSync(indexPath, index);
 // use the production versions of this dep by stripping the `/dev` path
 // find `es-react@____/dev` and replace with `es-react@____`
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
-index = index.replace(/(es-react@.*)\/dev/g, "$1");
-
-fs.writeFileSync(INDEX_PATH, index);
+const depsPath = path.join(__dirname, "../build/js/deps.js");
+let deps = fs.readFileSync(depsPath).toString();
+deps = deps.replace(/(es-react@.*)\/dev/g, "$1");
+fs.writeFileSync(depsPath, deps);
