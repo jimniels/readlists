b381fb91654a4b85dce17684c87737dd33e476c7
Jim Nielsen <jimniels@gmail.com>
2023-08-21 09:52:53 -0600

    feat: add link to reload with import= url

--- src/js/components/ExisitingReadlistMsg.js
+++ src/js/components/ExisitingReadlistMsg.js
@@ -5,30 +5,29 @@ export default function ExisitingReadlistMsg({ readlist }) {
   const parmas = new URLSearchParams(url.search);
   const importUrl = parmas.get("import");
 
-  if (!importUrl || !readlist) {
+  if (!importUrl) {
     return null;
   }
 
   return html`
     <div class="wrapper">
-      <div class="ImportMsg">
+      <div class="ExisitingReadlistMsg">
         <p>
           You tried to import a a Readlist via the URL but you already have one
-          open. Please save and/or delete the exisiting Readlist then reload the
-          page.
+          open. Please save and/or delete the exisiting Readlist then${" "}
+          <a href="">reload the page</a>.
         </p>
       </div>
       <style>
-        .ImportMsg.wrapper {
-          margin-top: 0;
-          margin-bottom: 0;
-        }
-        .ImportMsg p {
+        .ExisitingReadlistMsg p {
           background-color: var(--color-text);
           color: white;
           border-radius: var(--border-radius);
           padding: calc(var(--spacer)) calc(var(--spacer) * 2);
           margin: 0;
+        }
+        .ExisitingReadlistMsg a {
+          color: inherit;
         }
       </style>
     </div>
