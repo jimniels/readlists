d8bf8f1c024e24562b43617fc2d55fc82b3b607f
Jim Nielsen <jimniels@gmail.com>
2023-10-14 23:59:04 -0600

    chore: add jsdoc annotations

--- src/js/components/Textarea.js
+++ src/js/components/Textarea.js
@@ -1,20 +1,19 @@
 import { autosize, html, React } from "../deps.js";
 const { useEffect, useRef } = React;
 
+/** @param {any} props */
 export default function Textarea(props) {
-  const { children, ...rest } = props;
   const ref = useRef(null);
   useEffect(() => {
     autosize(ref.current);
   }, []);
 
+  /** @param {KeyboardEvent} e */
+  const onKeyDown = (e) => {
+    autosize(e.target);
+  };
+
   return html`
-    <textarea
-      ...${rest}
-      ref=${ref}
-      onKeyDown=${(e) => {
-        autosize(e.target);
-      }}
-    ></textarea>
+    <textarea ...${props} ref=${ref} onKeyDown=${onKeyDown}></textarea>
   `;
 }
